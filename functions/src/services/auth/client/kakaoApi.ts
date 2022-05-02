17f710e02c7aa231f428c421cd5b5b12f359778c
chanwoo <chocw0402@gmail.com>
2022-05-02 22:12:39 +0900

    [FIX] error message 수정

--- functions/src/services/auth/client/kakaoApi.ts
+++ functions/src/services/auth/client/kakaoApi.ts
@@ -4,7 +4,7 @@ const logger = require("../../../api/middlewares/logger");
 
 
 export async function KakaoAuthApi(
-    kakoAccessToken?: Token
+    kakaoAccessToken?: Token
 ): Promise<UserInfo | undefined> { // 제발 오류처리 어케 할거야
     try {
 
@@ -12,7 +12,7 @@ export async function KakaoAuthApi(
         const reqConfig = {
             headers: {
                 "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-                "Authorization": `Bearer ${kakoAccessToken}`
+                "Authorization": `Bearer ${kakaoAccessToken}`
             }
         };
 
@@ -29,15 +29,15 @@ export async function KakaoAuthApi(
         logger.appLogger.log({
             level: 'error',
             message: error.message
-        })
-        throw new Error("❌ AXIOS_ERROR ❌");
+        }); // FIXME 이놈은 서버에러인가?? 클라가 잘못된 토큰 보내준거 아닌가 ㅇㅅㅇ
+        throw new Error(`❌ AXIOS_ERROR : ${error.message} ❌`);
     }
 };
 
 
 
 export async function KakaoUnlinkApi(
-    kakoAccessToken?: Token
+    kakaoAccessToken?: Token
 ): Promise<void> {
     try {
 
@@ -45,7 +45,7 @@ export async function KakaoUnlinkApi(
         const reqConfig = {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": `Bearer ${kakoAccessToken}`
+                "Authorization": `Bearer ${kakaoAccessToken}`
             }
         };
 
