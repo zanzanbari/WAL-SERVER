d6715e9193111cd4ae312bd5fe7a7ba076d1fae7
chanwoo <chocw0402@gmail.com>
2022-05-07 05:52:56 +0900

    [ADD] 애플 공개키 요청

--- functions/src/services/auth/client/appleApi.ts
+++ functions/src/services/auth/client/appleApi.ts
@@ -1,15 +1,18 @@
 import jwt from "jsonwebtoken";
 import qs from "qs";
-import { IAppleUserInfo, Token } from "../../../interface/dto/response/authResponse";
+import { IApplePublicKeys, IAppleUserInfo, Token } from "../../../interface/dto/response/authResponse";
 import axios from "axios";
 const logger = require("../../../api/middlewares/logger");
 
-
-export async function getPublicKey() {
+// apple public key 가져오는 함수
+export async function getPublicKey(): Promise<IApplePublicKeys> {
 
     try {
 
-        const keys = await axios.get("https://appleid.apple.com/auth/keys");
+        const keys: IApplePublicKeys = await axios.get("https://appleid.apple.com/auth/keys")
+            .then(resolve => { return resolve.data["keys"] })
+            .catch(err => { return err });
+
         return keys;
         
     } catch (error) {
@@ -23,25 +26,8 @@ export async function getPublicKey() {
 }
 
 
-export async function appleAuthApi(id_token: string) {
-
-    try {
-
-
-
-    } catch (error) {
-        logger.appLogger.log({
-            level: "error",
-            message: error.message
-        });
-        throw new Error(error.message);
-    }
-}
-
-
 const appleApiUtil = {
-    getPublicKey,
-    appleAuthApi
+    getPublicKey
 };
 
 export default appleApiUtil;
