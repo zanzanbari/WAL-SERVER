d7cc33870271071981fa6696b6faf92ca0e11e64
chanwoo <chocw0402@gmail.com>
2022-05-06 21:39:58 +0900

    [ADD] 애플 로그인 서비스

--- functions/src/services/auth/appleAuthService.ts
+++ functions/src/services/auth/appleAuthService.ts
@@ -1,7 +1,8 @@
+import jwt from "jsonwebtoken";
 import IAuthService from "./authService";
 import { appleAuthApi } from "./client/appleApi";
 import { TokenDto } from "../../interface/dto/request/authRequest";
-import { AuthResponse, Token } from "../../interface/dto/response/authResponse";
+import { AuthResponse, IAppleUserInfo, Token } from "../../interface/dto/response/authResponse";
 import { issueAccessToken, issueRefreshToken } from "../../modules/tokenHandller";
 
 class AppleAuthService implements IAuthService {
@@ -11,11 +12,37 @@ class AppleAuthService implements IAuthService {
     ) {
     }
     
-    public async login(request: TokenDto) {
+    public async login(request: TokenDto): Promise<AuthResponse | undefined> {
       
       try {
-
         
+        const { aud: clientId } = jwt.decode(request.socialtoken) as IAppleUserInfo;
+        console.log("-----------");
+        console.log("client_id : ", clientId);
+        console.log("-----------");
+        const userData = await appleAuthApi(request.code as string, clientId as string);
+        console.log("-----------");
+        console.log("userData : ", userData);
+        console.log("-----------");
+        const refreshtoken = await issueRefreshToken();
+        console.log("-----------");
+        console.log("refreshtoken : ", refreshtoken);
+        console.log("-----------");
+        const socialUser = await this.userRepository.findByEmailOrCreateSocialUser("kakao", userData, request, refreshtoken);
+        console.log("-----------");
+        console.log("socialUser : ", socialUser);
+        console.log("-----------");
+        const accesstoken = await issueAccessToken(socialUser);
+        console.log("-----------");
+        console.log("accesstoken : ", accesstoken);
+        console.log("-----------");
+
+        const user: AuthResponse = {
+          nickname: socialUser.nickname,
+          accesstoken,
+          refreshtoken
+        }
+        return user;
         
       } catch (error) {
         console.error(error);
